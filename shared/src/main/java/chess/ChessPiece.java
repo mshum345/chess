9cfc39044e39b1c7e41117bf8ce153fa5899afc1
mshum345 <mitchell.shumway101@gmail.com>
2024-01-16 10:42:11 -0700

    Bishop first move rough draft.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -11,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ArrayList<ChessMove> validMoves = new ArrayList<ChessMove>();
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +53,53 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        if (type == PieceType.BISHOP) {
+            bishopMoves(myPosition);
+        }
+
+        return validMoves;
+    }
+
+    private void bishopMoves(ChessPosition myPosition) {
+        boolean upL = true;
+        boolean upR = true;
+        boolean downL = true;
+        boolean downR = true;
+        int i = 1;
+        PieceType TESTPromotionPiece = PieceType.QUEEN;
+
+        while(upL || upR || downL || downR) {
+            if (myPosition.getRow() + i < 9 && myPosition.getColumn() - i > 0) {
+                validMoves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() - i), TESTPromotionPiece));
+            }
+            else {
+                upL = false;
+            }
+
+            if (myPosition.getRow() + i < 9 && myPosition.getColumn() + i < 9) {
+                validMoves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() + i), TESTPromotionPiece));
+            }
+            else {
+                upR = false;
+            }
+
+            if (myPosition.getRow() - i > 0 && myPosition.getColumn() - i > 0) {
+                validMoves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() - i), TESTPromotionPiece));
+            }
+            else {
+                downL = false;
+            }
+
+            if (myPosition.getRow() - i > 0 && myPosition.getColumn() + i < 9) {
+                validMoves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() + i), TESTPromotionPiece));
+            }
+            else {
+                downR = false;
+            }
+            
+            i ++;
+        }
+
+        return;
     }
 }
