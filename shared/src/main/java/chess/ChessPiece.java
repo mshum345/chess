7605021136963757b8fde0fb8c15168afabf2deb
mshum345 <mitchell.shumway101@gmail.com>
2024-01-17 17:33:02 -0700

    Added toStrings

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -952,4 +952,12 @@ public class ChessPiece {
     public int hashCode() {
         return Objects.hash(pieceColor, type);
     }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
+    }
 }
