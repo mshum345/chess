a81157fb15d451c0520dcae26eb4456d9979b36d
mshum345 <mitchell.shumway101@gmail.com>
2024-02-01 12:23:46 -0700

    removed unused import statement, FINISH PHASE 1

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,7 +3,6 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
