99556e1de0d62ac1432f86b7188d90a867584db2
mshum345 <mitchell.shumway101@gmail.com>
2024-02-01 11:48:44 -0700

    All but 2 tests working.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
