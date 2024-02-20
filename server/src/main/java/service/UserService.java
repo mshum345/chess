f13d105f692b65b6943864a1cfb3ed1391d0983f
mshum345 <mitchell.shumway101@gmail.com>
2024-02-20 12:09:16 -0700

    Added comments and started UserService (previous commit also added ClearServiceTests rough draft)

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -9,19 +9,20 @@ import java.util.UUID;
 
 public class UserService {
     private UserDAO dataAccess;
-    private UserData userData;
 
     public UserService(UserDAO dataAccess) {
         this.dataAccess = dataAccess;
     }
 
-    public AuthData register() throws DataAccessException {
+    public AuthData register(UserData userData) throws DataAccessException {
         var checkUser = dataAccess.getUser(userData);
 
+        // Check if username has already been taken
         if (checkUser != null) {
             throw new DataAccessException("Error: already taken");
         }
 
+        // Generate AuthData and insert into users and auths
         var authToken = UUID.randomUUID().toString();
         var authData = new AuthData(authToken, userData.username());
         dataAccess.addUser(userData);
